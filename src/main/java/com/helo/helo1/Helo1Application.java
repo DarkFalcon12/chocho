6c185503ad335c450c812408052616adf20ca5ae
Almatheen 🌚 <166009370+DarkFalcon12@users.noreply.github.com>
2025-09-04 00:24:55 -0400

    Add files via upload

---
+++ src/main/java/com/helo/helo1/Helo1Application.java
@@ -0,0 +1,13 @@
+package com.helo.helo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Helo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Helo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/helo/helo1/Helo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helo.helo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Helo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
